6f0d0d5471c11b9776a362d9e523618f2aaf8c95
Evgeniy Zudin <eezudin87@gmail.com>
2020-09-24 19:24:07 +0300

    First tests for Point

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandox.Point;
+
+public class PointTests {
+
+  @Test
+  public void pointTest1() {
+    Point p1 = new Point(3, 5);
+    double expectedValue = 4.123105625617661;
+    Assert.assertEquals(p1.distance(new Point(7, 4)), expectedValue);
+  }
+
+  @Test
+  public void pointTest2() {
+    Point p1 = new Point(2, 2);
+    double expectedValue = 0.0;
+    Assert.assertEquals(p1.distance(new Point(2, 2)), expectedValue);
+  }
+
+}
