dd5944b2e9e1eaf2f1a2335be357f323a5fd1f3f
Evgeniy Zudin <eezudin87@gmail.com>
2020-10-11 14:18:31 +0300

    navigationHelper's method optimized.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,14 +10,29 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    } else {
+      click(By.linkText("groups"));
+    }
   }
 
   public void goToCreateContactPage() {
-    click(By.xpath(".//a[.='add new']"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+      return;
+    } else {
+      click(By.xpath(".//a[.='add new']"));
+    }
   }
 
   public void goToHomePage() {
-    click(By.xpath(".//a[.='home']"));
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    } else {
+      click(By.xpath(".//a[.='home']"));
+    }
   }
 }
