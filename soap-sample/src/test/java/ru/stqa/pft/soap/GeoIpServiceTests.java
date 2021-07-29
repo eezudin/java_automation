2242c6a6b26630f8243a0f32a2d316b041801865
Evgeniy Zudin <eezudin87@gmail.com>
2021-07-29 16:27:34 +0300

    update 'testMyIp'

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.soap;
+
 import com.lavasoft.GeoIPService;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.regex.Pattern;
 
 import static org.testng.Assert.assertTrue;
@@ -10,7 +14,26 @@ public class GeoIpServiceTests {
 
   @Test
   public void testMyIp() {
-    String geoIp = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("89.109.44.203");
+    String geoIp = new GeoIPService().getGeoIPServiceSoap12().getIpLocation(getSystemIP());
     assertTrue(Pattern.compile("<Country>RU</Country>").matcher(geoIp).find());
   }
+
+
+  private String getSystemIP() {
+    // Find public IP address
+    String systemIpAddress;
+    try {
+      URL url_name = new URL("http://bot.whatismyipaddress.com");
+
+      try (BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream()))) {
+
+        // reads system IPAddress
+        systemIpAddress = sc.readLine().trim();
+      }
+    } catch (Exception e) {
+      systemIpAddress = "Cannot Execute Properly";
+    }
+    return systemIpAddress;
+  }
+
 }
